a44c36c47647552cf9d9c580c7dd23bb65cdf57a
Chengruidong Zhang <chengzhang@microsoft.com>
2023-12-13 08:24:23 +0000

    Update eval settings

--- scripts/v1_5/eval/textvqa.sh
+++ scripts/v1_5/eval/textvqa.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 python -m llava.eval.model_vqa_loader \
-    --model-path liuhaotian/llava-v1.5-13b \
+    --model-path /home/chengzhang/models/llava/llava-v1.5-7b \
     --question-file ./playground/data/eval/textvqa/llava_textvqa_val_v051_ocr.jsonl \
-    --image-folder ./playground/data/eval/textvqa/train_images \
-    --answers-file ./playground/data/eval/textvqa/answers/llava-v1.5-13b.jsonl \
+    --image-folder /home/chengzhang/datasets/TextVQA/images-v0.5/train_images \
+    --answers-file ./playground/data/eval/textvqa/answers/llava-v1.5-7b.jsonl \
     --temperature 0 \
     --conv-mode vicuna_v1
 
 python -m llava.eval.eval_textvqa \
-    --annotation-file ./playground/data/eval/textvqa/TextVQA_0.5.1_val.json \
-    --result-file ./playground/data/eval/textvqa/answers/llava-v1.5-13b.jsonl
+    --annotation-file /home/chengzhang/datasets/TextVQA/annotation/TextVQA_0.5.1_val.json \
+    --result-file ./playground/data/eval/textvqa/answers/llava-v1.5-7b.jsonl
